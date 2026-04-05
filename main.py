48927ccdff5e37e871d751886a08b8dc62b3f6f5
Keven Augusto <kevenbezerra@gmail.com>
2026-04-05 00:31:04 -0300

    Feat: add a new step in export processing

--- main.py
+++ main.py
@@ -14,17 +14,19 @@ ENTER = 'enter'
 ASSETS = 'assets'
 
 ARQUIVOS_SALVOS = cv2.imread(str(Path(__file__).parent / ASSETS / 'Arquivos Salvos.png'), 0)
+LEMBRETES = cv2.imread(str(Path(__file__).parent / ASSETS / 'Lembretes.png'), 0)
 LOCAL_DO_SALVAMENTO = cv2.imread(str(Path(__file__).parent / ASSETS / 'Local do Salvamento.png'), 0)
 MENU = cv2.imread(str(Path(__file__).parent / ASSETS / 'Menu.png'), 0)
 NADA_CONSTA = cv2.imread(str(Path(__file__).parent / ASSETS / 'Nada Consta.png'), 0)
+OK = cv2.imread(str(Path(__file__).parent / ASSETS / 'OK.png'), 0)
 PASTA_JURIDICA = cv2.imread(str(Path(__file__).parent / ASSETS / 'PJ.png'), 0)
 SALVAR_EM_DISCO = cv2.imread(str(Path(__file__).parent / ASSETS / 'Salvar em Disco.png'), 0)
 SUMARIO = cv2.imread(str(Path(__file__).parent / ASSETS / 'Sumário.png'), 0)
 VOLTAR = cv2.imread(str(Path(__file__).parent / ASSETS / 'Voltar.png'), 0)
 
-ULTIMO_VALOR_DE_PJ = 4500
+ULTIMO_VALOR_DE_PJ = 1500
 TENTATIVAS_DE_LOCALIZAR = 3
-INTERVALO_ENTRE_TENTATIVAS = 5
+INTERVALO_ENTRE_TENTATIVAS = 3
 
 LOG = logging.getLogger('migracao')
 pyautogui.PAUSE = 0.5
@@ -44,12 +46,12 @@ def setup_logging() -> None:
         logging.basicConfig(level=logging.INFO)
 
 
-def locate_on_screen(image: cv2.typing.MatLike, screen: MSSBase) -> tuple[float, float, float, float]:
-    for attempt in range(1, TENTATIVAS_DE_LOCALIZAR + 1):
+def locate_on_screen(image: cv2.typing.MatLike, screen: MSSBase, max_attempts: int = TENTATIVAS_DE_LOCALIZAR) -> tuple[float, float, float, float]:
+    for attempt in range(1, max_attempts + 1):
         found_coord = AutomationUtils.find_template(image, screen)
         if found_coord is not None:
             return found_coord
-        if attempt < TENTATIVAS_DE_LOCALIZAR:
+        if attempt < max_attempts:
             LOG.debug(f'Imagem NÃO Encontrada: aguardando {INTERVALO_ENTRE_TENTATIVAS} segundos.')
             time.sleep(INTERVALO_ENTRE_TENTATIVAS)
     raise ImageNotFound('A Imagem Solicitada NÃO foi Encontrada!')
@@ -69,9 +71,12 @@ def main(contador_de_pj: int, busca_sequencial: bool, busca_nao_sequencial: list
         try:
             while True:
                 if busca_sequencial and contador_de_pj > ULTIMO_VALOR_DE_PJ:
-                    LOG.debug(f'Pastas Jurídias NÃO Encontradas: {nao_encontradas}')
                     LOG.info('Encerrando as Exportações com SUCESSO!')
                     break
+                if len(nao_encontradas) >= 100:
+                    LOG.debug(f'Pastas Jurídicas NÃO Encontradas: {nao_encontradas}')
+                    LOG.debug('Efetuando a Limpeza das Pastas Jurídicas NÃO encontradas.')
+                    nao_encontradas = []
                 LOG.info(f'Iniciando o Processo de Exportação para a Pasta Jurídica: {contador_de_pj}')
                 x, y, w, h = locate_on_screen(PASTA_JURIDICA, default_screen)
                 mouse_coord = AutomationUtils.move_mouse_to((x + w, y, 100.0, h))
@@ -91,6 +96,14 @@ def main(contador_de_pj: int, busca_sequencial: bool, busca_nao_sequencial: list
                     continue
                 except ImageNotFound:
                     pyautogui.press(ENTER)
+                try:
+                    locate_on_screen(LEMBRETES, default_screen)
+                    LOG.debug('Encontrada uma TELA de Lembrete!')
+                    found_coord = locate_on_screen(OK, default_screen)
+                    mouse_coord = AutomationUtils.move_mouse_to(found_coord)
+                    pyautogui.click(mouse_coord)
+                except ImageNotFound:
+                    pass
                 found_coord = locate_on_screen(MENU, default_screen)
                 mouse_coord = AutomationUtils.move_mouse_to(found_coord)
                 pyautogui.click(mouse_coord)
@@ -103,7 +116,7 @@ def main(contador_de_pj: int, busca_sequencial: bool, busca_nao_sequencial: list
                 mouse_coord = AutomationUtils.move_mouse_to(found_coord)
                 pyautogui.click(mouse_coord)
                 pyautogui.press(ENTER)
-                locate_on_screen(ARQUIVOS_SALVOS, default_screen)
+                locate_on_screen(ARQUIVOS_SALVOS, default_screen, 40)
                 pyautogui.press(ENTER)
                 LOG.info(f'A Pasta Jurídica {contador_de_pj} foi Exportada com SUCESSO!')
                 found_coord = locate_on_screen(VOLTAR, default_screen)
@@ -114,17 +127,17 @@ def main(contador_de_pj: int, busca_sequencial: bool, busca_nao_sequencial: list
                 else:
                     contador_de_pj = busca_nao_sequencial.pop()
         except IndexError:
-            LOG.debug(f'Pastas Jurídias NÃO Encontradas: {nao_encontradas}')
             LOG.info('Todas as Pastas Jurídicas Informadas foram PROCESSADAS!')
         except (ImageNotFound, cv2.error) as error:
             LOG.info('Houve um Problema durante o Fluxo de Exportação!')
             LOG.debug(error)
         except KeyboardInterrupt:
-            LOG.debug(f'Pastas Jurídias NÃO Encontradas: {nao_encontradas}')
             LOG.info('Encerrando as Exportações com SUCESSO!')
+        finally:
+            LOG.debug(f'Pastas Jurídicas NÃO Encontradas: {nao_encontradas}')
 
 
 if __name__ == '__main__':
     setup_logging()
     time.sleep(INTERVALO_ENTRE_TENTATIVAS)
-    main(1, True, [1, 2, 3, 4, 5])
+    main(480, True, [1, 2, 3, 4, 5])
